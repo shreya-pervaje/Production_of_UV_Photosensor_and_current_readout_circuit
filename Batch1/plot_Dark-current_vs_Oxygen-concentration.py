133437a3a502b8bbc75772e8bdd7e4864a55d90a
shreya-pervaje <pervaje.shreya@gmail.com>
2023-12-27 14:21:50 +0100

    Updated code

--- Batch1/plot_Dark-current_vs_Oxygen-concentration.py
+++ Batch1/plot_Dark-current_vs_Oxygen-concentration.py
@@ -3,38 +3,48 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from scipy.constants import constants
 import pandas as pd
-#import xlrd
 import os
 import sys
+import matplotlib as mpl
 
 def linear(x, a, b):
     return a * x + b
+
+#image settings
+cm = 1/2.54
+plt.figure(figsize= (7.3*cm,5*cm),dpi=600)
+settings = {"xtick.labelsize": 6,
+            "ytick.labelsize": 6,
+            "font.size": 9,
+            "legend.fontsize": 6,
+            "font.family":['Arial']
+            }
+mpl.rcParams.update(settings)
 
 AnzahlTransistorenInSerienmessung = 4
 
 colors = ['blue','red','yellow','green']
 dataname = 'DarkCurrent_vs_OxygenConc.xlsx'
-U_Drain = pd.read_excel(dataname, sheet_name="Data", usecols=[1],engine='openpyxl')
 
-        
+# to read the data from excel sheet
+U_Drain = pd.read_excel(dataname, sheet_name="Data", usecols=[1],engine='openpyxl')
 I_Drain = pd.read_excel(dataname, sheet_name="Data", usecols=[0],engine='openpyxl')
 
 U_Drain = np.array(U_Drain.T)[0]
 I_Drain = np.array(I_Drain.T)[0]
 
 plt.plot(U_Drain,np.abs(I_Drain),color = colors[0],label = '9% Oxygen')
-#plt.show()
-#sys.exit()
+
+# to read different columns of excel file and plot
 j = 1
 while j < AnzahlTransistorenInSerienmessung:
     appends = "Append" + str(j)
     j = j + 1
 
-    U_Gate = pd.read_excel(dataname, sheet_name=appends, usecols=[1],engine='openpyxl')
-
+    U_Drain = pd.read_excel(dataname, sheet_name=appends, usecols=[1],engine='openpyxl')
     I_Drain = pd.read_excel(dataname, sheet_name=appends, usecols=[0],engine='openpyxl')
 
-    U_Gate = np.array(U_Gate.T)[0]
+    U_Drain = np.array(U_Drain.T)[0]
     I_Drain = np.array(I_Drain.T)[0]
     
     if j == 2:
@@ -51,10 +61,9 @@ while j < AnzahlTransistorenInSerienmessung:
    
 plt.xlabel('Voltage $U$ (V)')
 plt.ylabel('Current $I$ (A)')
-
 plt.legend()
 plt.yscale('log')
-#plt.show()
-#plt.savefig('plot.png')
-#plt.title(dataname)
+
+plt.savefig('plot_dark_current_vs_oxygen_concentration.png')
+
 
